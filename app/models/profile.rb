406c43f377be9078a0963ead4c9b26b74b4e410c
Jerry Gao <gao@blackwright.com>
2017-02-17 14:35:22 -0500

    update profile model for avatar and cover

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user, :inverse_of => :profile
+  belongs_to :avatar, :class_name => "Photo",
+                      :optional => true
+  belongs_to :cover, :class_name => "Photo",
+                     :optional => true
 end
