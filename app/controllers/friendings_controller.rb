e79222c937cffbafb4e39d3f4a99dffa923209de
Jerry Gao <gao@blackwright.com>
2017-02-16 22:03:47 -0500

    friendings create action

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -1,2 +1,13 @@
 class FriendingsController < ApplicationController
+
+  def create
+    if new_friend = User.find_by_id(params[:id])
+      current_user.friended_users << new_friend
+      flash[:success] = "You're now friends with #{new_friend.full_name}"
+      redirect_back(fallback_location: user_path(new_friend))
+    else
+      flash[:error] = "User not found"
+      redirect_back(fallback_location: user_path(current_user))
+    end
+  end
 end
