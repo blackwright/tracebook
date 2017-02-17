861b35207a2aacdface2d24f8c98e702fccac3d4
Jerry Gao <gao@blackwright.com>
2017-02-16 21:53:02 -0500

    add view helpers

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,37 @@
 module CommentsHelper
+
+  def comment_feedback(likeable)
+    num_likers = likeable.likers.size
+    if num_likers > 0
+      result = ""
+      if likeable.likers.include?(current_user)
+        result << "You "
+        num_others = num_likers - 1
+        result << "and #{pluralize(num_others, 'person')} " if num_others > 0
+        result << "like this."
+      else
+        result << "#{pluralize(num_likers, 'person')} "
+        result << "#{num_likers > 1 ? 'like' : 'likes'} this."
+      end
+    else
+      nil
+    end
+  end
+
+  def render_delete_link(comment)
+    if current_user == comment.author
+      if comment.commentable.class == Post
+        path = user_post_comment_path(comment.author.id,
+                                      comment.commentable.id,
+                                      comment.id)
+      else
+        path = user_photo_comment_path(comment.author.id,
+                                       comment.commentable.id,
+                                       comment.id)
+      end
+      content_tag(:span, class: "inline pull-right") do
+        link_to("Delete", path, :method => :delete)
+      end
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,31 @@
 module UsersHelper
+
+  def render_profile_or_friend_link(user)
+    if current_user == user
+      link_to "Edit Profile", edit_user_profile_path(current_user),
+                              id: "profile-link", class: "right"
+    else
+      if current_user.friends?(user)
+        link_to "Unfriend", friending_path(user),
+                            :method => :delete,
+                            id: "profile-link",
+                            class: "btn btn-primary right"
+      else
+        link_to "+ Friend", friendings_path(:id => user.id),
+                            :method => :post,
+                            id: "profile-link",
+                            class: "btn btn-primary right"
+      end
+    end
+  end
+
+  def render_friend_or_unfriend(user)
+    if current_user.all_friends.include?(user)
+      link_to "Unfriend", friending_path(user),
+                          :method => :delete, class: "btn btn-default right"
+    else
+      link_to "+ Friend", friendings_path(:id => user.id),
+                        :method => :post, class: "btn btn-primary right"
+    end
+  end
 end
