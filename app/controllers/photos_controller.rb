1d89ac2a7ab34284ed6eb597c0c2707c8dd9f58f
Jerry Gao <gao@blackwright.com>
2017-02-17 14:04:52 -0500

    photos new/create actions

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,19 @@
 class PhotosController < ApplicationController
+  before_action :set_user
+  before_action :require_current_user, except: [:show, :index]
+
+  def new
+    @photo = current_user.photos.build
+  end
+
+  def create
+    @photo = current_user.photos.build(photo_params)
+    if @photo.save
+      flash[:success] = "Photo uploaded!"
+      redirect_to user_photo_path(@user, @photo)
+    else
+      flash.now[:error] = "Couldn't upload photo"
+      render :new
+    end
+  end
 end
