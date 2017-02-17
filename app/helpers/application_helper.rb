a82ba737896a472ab4e20921ba75f3be92bca276
Jerry Gao <gao@blackwright.com>
2017-02-16 21:11:44 -0500

    profile edit and show views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def out(value)
+    value.blank? ? "--" : value
+  end
+
+  def date_to_string(date)
+    "#{date.strftime('%B %-d, %Y')}" if date
+  end
+
+  def nav_link_class(path)
+    "active" if current_page?(path)
+  end
 end
