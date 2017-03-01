08a487efc56304b2b2bca7f5805d28c64f45582e
Jerry Gao <gao@blackwright.com>
2017-03-01 12:16:40 -0500

    update seeds for friend requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,28 +45,35 @@ end
 
 puts "Seeding comments..."
 Post.all.each do |post|
-  (rand 0..MULTIPLIER).times do
+  (rand 0..MULTIPLIER / 2).times do
     comment = post.comments.build(user_id: User.pluck(:id).sample,
                                   body: Faker::Lorem.paragraph)
     comment.save!
   end
 end
 
-puts "Seeding friendships..."
+puts "Seeding friendings..."
 User.all.each do |user|
-  num_friends = rand(0..MULTIPLIER)
+  num_requests = rand(0..MULTIPLIER)
   all_friend_ids = User.where.not(:id => user.id).pluck(:id)
-  until user.friended_users.count == num_friends
+  until user.requested_users.count == num_requests
     new_friend = User.find_by_id(all_friend_ids.shuffle!.pop)
-    user.friended_users << new_friend unless user.users_friended_by.include?(new_friend)
+    unless user.users_requested_by.include?(new_friend)
+      user.requested_users << new_friend
+    end
   end
 end
 
+puts "Accepting friend requests..."
+Friending.all.each do |friending|
+  confirm = [true, false].sample
+  friending.update!(:accepted => true) if confirm
+end
 
 puts "Seeding post likes..."
 Post.all.each do |post|
   all_user_ids = User.pluck(:id)
-  (rand 0..MULTIPLIER).times do
+  (rand 0..MULTIPLIER / 2).times do
     like = post.likes.build(user_id: all_user_ids.shuffle!.pop)
     like.save!
   end
@@ -75,14 +82,14 @@ end
 puts "Seeding comment likes..."
 Comment.all.each do |comment|
   all_user_ids = User.pluck(:id)
-  (rand 0..MULTIPLIER).times do
+  (rand 0..MULTIPLIER / 2).times do
     like = comment.likes.build(user_id: all_user_ids.shuffle!.pop)
     like.save!
   end
 end
 
-puts "Seeding blank user..."
-user = User.new(email: "user0@email.com",
+puts "Seeding guest user..."
+user = User.new(email: "guest@email.com",
                 password: "password",
                 first_name: "Blank",
                 last_name: "User")
