6707345860ebf68bf4ecfdfe4b47de67e07f7124
Jerry Gao <gao@blackwright.com>
2017-03-02 14:35:42 -0500

    update factories for likes and comments

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,24 +7,65 @@ FactoryGirl.define do
     password   "password"
   end
 
+  factory :profile do
+    user
+
+    trait :filled_out do
+      birthday "11/10/1987"
+      gender   "F"
+      college  "Hofstra University"
+      hometown "Great Neck"
+      location "Marathon Parkway"
+      phone    "123-456-7890"
+      tagline  "Just Do It"
+      about    "Just a small town girl"
+    end
+  end
+
   factory :post do
     body "Lorem ipsum dolor sit amet."
     author
   end
 
-  factory :comment do
-    body "Lorem ipsum dolor sit amet."
+  factory :photo do
     author
-
-    trait :for_post do
-      commentable_type "Post"
-      association :commentable, :factory => :post
-    end
-
-    trait :for_photo do
-      commentable_type "Photo"
-      association :commentable, :factory => :photo
-    end
   end
 
+  factory :post_comment, :class => "Comment" do
+    body "Lorem ipsum dolor sit amet."
+    commentable_type "Post"
+    association :commentable, :factory => :post
+    author
+  end
+
+  factory :photo_comment, :class => "Comment" do
+    body "Lorem ipsum dolor sit amet."
+    commentable_type "Photo"
+    association :commentable, :factory => :photo
+    author
+  end
+
+  factory :post_like, :class => "Like" do
+    likeable_type "Post"
+    association :likeable, :factory => :post
+    association :liker, :factory => :user
+  end
+
+  factory :photo_like, :class => "Like" do
+    likeable_type "Photo"
+    association :likeable, :factory => :photo
+    association :liker, :factory => :user
+  end
+
+  factory :post_comment_like, :class => "Like" do
+    likeable_type "Comment"
+    association :likeable, :factory => :post_comment
+    association :liker, :factory => :user
+  end
+
+  factory :photo_comment_like, :class => "Like" do
+    likeable_type "Comment"
+    association :likeable, :factory => :photo_comment
+    association :liker, :factory => :user
+  end
 end
