ffa0c421ce627d3f85d522ae5e17fc004a64c06c
Jerry Gao <gao@blackwright.com>
2017-03-05 15:11:43 -0500

    add PostsModule

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,34 @@
+var TB = TB || {};
+
+TB.PostsModule = ( function() {
+  
+  let _getPostsContainer = () => {
+    return $('#posts-container');
+  };
+
+  let _clearForm = () => {
+    $('#new_post')[0].reset();
+  };
+
+  let _getPost = (id) => {
+    return $(`[data-id="${id}"][data-type="post"]`);
+  };
+
+  let addPost = (post) => {
+    let $post = $(post);
+    let $container = _getPostsContainer();
+    $container.prepend($post);
+    _clearForm();
+  };
+
+  let removePost = (id) => {
+    let $post = _getPost(id);
+    $post.remove();
+  };
+
+  return {
+    addPost: addPost,
+    removePost: removePost
+  };
+
+})();
