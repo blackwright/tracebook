0286b7df1e4509a8a01bda109ce4ae6454edd4ce
Jerry Gao <gao@blackwright.com>
2017-03-11 15:54:28 -0500

    clear timeouts for multiple flash notices

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -4,5 +4,10 @@ $(document).ready( function() {
     $('#flash').css('top', Math.max(0, 50 - $(this).scrollTop()));
   });
 
-  $('#flash').show().delay(2000).slideUp(250);
+  var flashTimeout;
+  clearTimeout(flashTimeout);
+  $('#flash').show();
+  flashTimeout = setTimeout( function() {
+    $('#flash').slideUp(250);
+  }, 2000);
 });
